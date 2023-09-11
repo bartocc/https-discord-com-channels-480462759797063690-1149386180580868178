dce826e3c9d5fccbfe8e4dbf3582ea35851760b2
Julien Palmas <julien@crx.io>
2023-09-11 14:19:07 +0200

    template registry

--- packages/my-app/types/global.d.ts
+++ packages/my-app/types/global.d.ts
@@ -1,1 +1,8 @@
 import '@glint/environment-ember-loose';
+
+import type MyAddonRegistry from 'my-addon/template-registry';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry
+    extends MyAddonRegistry /* other addon registries */ {}
+}
